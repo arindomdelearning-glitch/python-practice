48bd9fa40c16a972fb8195cc46c80462c8e6fbfa
Arindom Mukherjee <arindommukherjee@Arindoms-MacBook-Air.local>
2025-12-27 00:26:44 +0530

    Add Python input/output basics

---
+++ basics/Input_Output_in_Python.py
@@ -0,0 +1,24 @@
+"""
+This chaptter is for learning the below:
+1.  How to take input
+2.  Input type
+3.  type casting
+4.  Command line argument
+"""
+
+
+a = input('Enter a value')
+print(a)
+print(type(a))
+
+
+a1 = bool(input('Enter a value'))
+print(type(a1))
+print(a1)
+
+
+
+a2 = eval(input('Enter a value'))
+print(type(a2))
+print(a2)
+
